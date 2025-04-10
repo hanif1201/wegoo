d6906ff54fee067a3cb1498ae6952d4985c8abb8
Hanif1201 <adeyemihanif@gmail.com>
2025-04-10 19:55:22 +0100

    Revert "updated"
    This reverts commit 772b82473f97e82201382828fbf4f80a63cfdc33.

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 // Use your actual API URL
-const API_URL = "http://localhost:5000";
+const API_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://https://wegoo.onrender.com" // Replace with your actual Render URL
+    : "http://localhost:5000";
 
 const api = axios.create({
   baseURL: API_URL,
